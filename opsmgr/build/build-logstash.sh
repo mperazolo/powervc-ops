e278fa5c54e8adffc84baf95a4f6c1c50c1692b7
Marcelo Perazolo <mperazol@us.ibm.com>
2021-01-29 14:45:32 -0500

    use static keys to avoid network problems with gpg keyserver
    Signed-off-by: Marcelo Perazolo <mperazol@us.ibm.com>

--- opsmgr/build/build-logstash.sh
+++ opsmgr/build/build-logstash.sh
@@ -6,6 +6,7 @@ export OSS=true
 export LOGSTASH_SOURCE=1
 export LOGSTASH_PATH="$SCRIPT_PATH/logstash"
 export PATCH_PATH="$SCRIPT_PATH/patch/logstash"
+export KEYS_PATH="$SCRIPT_PATH/keys/rvm"
 
 git clone https://github.com/elastic/logstash.git
 cd logstash
@@ -15,7 +16,8 @@ patch < $PATCH_PATH/build.gradle.patch
 cd rakelib; patch < $PATCH_PATH/artifacts.rake.patch; cd ..
 
 echo "jruby-9.2.9.0" > .ruby-version
-gpg --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+gpg --import $KEYS_PATH/409B6B1796C275462A1703113804BB82D39DC0E3.key
+gpg --import $KEYS_PATH/7D2BAF1CF37B13E2069D6956105BD0E739499BDB.key
 curl -sSL https://get.rvm.io | bash -s stable --ruby=$(cat .ruby-version)
 source ~/.rvm/scripts/rvm
 
