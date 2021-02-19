2a27de87cebce5709b0cd7562e7018752bbc0afd
Marcelo Perazolo <mperazol@us.ibm.com>
2021-02-19 12:57:30 -0500

    fix build-prereqs.sh to support both x86_64 and ppc64le
    Signed-off-by: Marcelo Perazolo <mperazol@us.ibm.com>

--- opsmgr/build/build-prereqs.sh
+++ opsmgr/build/build-prereqs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+REPOURL="https://www.rpmfind.net/linux/centos/8-stream/PowerTools/`uname -m`/os/Packages"
+GIFLIB="giflib-devel-5.1.4-3.el8.`uname -m`.rpm"
+
 sudo yum -y install make rpm-build gpg
 sudo yum -y install gcc gcc-c++
 sudo yum -y install java-1.8.0-openjdk-devel java-11-openjdk-devel
 sudo yum -y install ruby ruby-devel rubygems
 sudo yum -y install cairo cairo-devel libjpeg-turbo-devel pango pango-devel
-sudo yum -y install https://www.rpmfind.net/linux/centos/8-stream/PowerTools/ppc64le/os/Packages/giflib-devel-5.1.4-3.el8.ppc64le.rpm
+sudo yum -y install ${REPOURL}/${GIFLIB}
 echo "% _binaries_in_noarch_packages_terminate_build 0" | sudo tee /etc/rpm/macros
 
